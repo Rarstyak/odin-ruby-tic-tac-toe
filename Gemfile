3bc9423f77d64b339ca04f4c6ef938cd182a6cc9
Rarstyak <rarstyak@gmail.com>
2023-10-17 16:13:55 -0400

    undo bundle

--- Gemfile
+++ Gemfile
@@ -2,11 +2,6 @@
 
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in odin-ruby-tic-tac-toe.gemspec
-gemspec
-
-gem "rake", "~> 13.0"
-
-gem "rspec", "~> 3.0"
-
-gem "rubocop", "~> 1.21"
+# gem "rails"
+gem 'rspec', '~> 3.12'
+gem 'rubocop', '~> 1.57', require: false
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,9 +2,27 @@ require_relative '../lib/cell'
 
 describe Cell do
 
-  subject(:cell) { described_class.new }
+  subject(:default_cell) { described_class.new }
 
-  describe '#set' do
+  describe '#initialize' do
+    it 'has dash mark' do
+      expect(default_cell.mark).to eq('-')
+    end
   end
 
+  describe '#set' do
+    it 'changes mark' do
+      new_mark = 'X'
+      expect { default_cell.set(new_mark) }.to change { default_cell.mark }.to(new_mark)
+    end
+  end
+
+  describe '#clear' do
+    it 'resets mark' do
+      new_mark = 'X'
+      default_cell.set(new_mark)
+      default_cell.clear
+      expect(default_cell.mark).to eq('-')
+    end
+  end
 end
